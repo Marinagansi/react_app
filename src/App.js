c6cc42ed0bf6456bfd2fe6974c7da2154f9702df
Marinagansi <78782374+Marinagansi@users.noreply.github.com>
2023-01-11 08:29:32 +0545

    database

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import { useState } from 'react';
-import Feedback from './components/Feedback';
-import Button from './components/Button';
+
+import { useState ,useEffect} from 'react';
+import axios from "axios";
 import './App.css';
  
-function App({name,age}) {
- const [counter, setCounter]=useState(0)
- //setTimeout(() =>setCounter(counter+1),1000)
- console.log(`Rendering ${counter}...`)
- const handlePlus=()=>{
-  setCounter(counter+1)
- }
- const handleminus=()=>{
-  setCounter(counter-1)
- }
+function App(props) {
+  const[newNote,setNewNote]=useState('')
+  const[notes,setNotes]=useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3002/notes')
+    .then((response)=>{
+      console.log(response)
+      setNotes(response.data)
+    })
+  },[])
+const handleInputChange=(event)=>{
+console.log(event.target.value)
+setNewNote(event.target.value)  
+}
+const handleadd=(event)=>{
+  event.preventDefault()
+  alert('Testing..')
+}
   return (
     <>
-    {/* <h2>Hello {name}, you're{age}years old</h2>
-    <h2>{counter}</h2>
-    <button onClick={handlePlus}>plus</button>
-    <button onClick={handleminus}>Minus</button> */}
-<br/>
-    
-      <Button/>
-    {/* <Feedback/> */}
+   <h2>Notes</h2>
+   <ul>
+     {notes.map(note=>
+     <li key={note.id}>
+      {note.content}
+      </li>)} 
+   </ul>
+   <form>
+    <input value={newNote} onChange={handleInputChange}/>
+    <button onClick={handleadd}>add</button>
+   </form>
+
     
     </>
 
